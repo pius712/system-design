3309bae54bfc1930819af23be723ac05988340c1
pius712 <aksfbsgnlfls@gmail.com>
2024-11-10 21:48:22 +0900

    first commit

---
+++ src/main/kotlin/com/pius/SystemDesignApplication.kt
@@ -0,0 +1,11 @@
+package com.pius
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SystemDesignApplication
+
+fun main(args: Array<String>) {
+    runApplication<SystemDesignApplication>(*args)
+}
